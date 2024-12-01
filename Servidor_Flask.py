f4403fde713cb51688a43e1ff8deb50177fce135
FelipeC074 <felipecarabena@gmail.com>
2024-11-30 22:11:11 -0300

    Inicio De Sesion

--- Servidor_Flask.py
+++ Servidor_Flask.py
@@ -3,45 +3,88 @@
 #Para Inicializar:
 #1 Escribir set nombre para server = RutaAbsolutaDeEsteArchivo.py
 #2 Escribir flask run
-from flask import Flask, request, redirect, url_for
+from flask import  request, Flask
+import json
 
 ArchivoHTML = open("PaginaWeb.html", "r")
-Texto_Inicio = ArchivoHTML.read()
+Inter = ArchivoHTML.readlines()
+ArchivoHTML.close()
+Texto = ""
+for i in Inter:
+    Texto += i
+ArchivoXML = open("Textos.xml", "r")
+Interin = ArchivoXML.readlines()
+ArchivoXML.close()
+Textos = ""
+for i in Interin:
+    Textos += i
+
 #Sale error si se returna ArchivoHTML
-#Es correcto returnar Texto_Inicio
-Server = Flask(__name__)
+#Es correcto returnar Texto
+Server = Flask(__name__, template_folder = "Programacion_BackEnd")  #El primer parametro especifica que archivo será la app
+                                                                    #El segundo parametro especifica donde buscar los templates
 
-@Server.route("/")
-def Funcion_Init():
-    return Texto_Inicio
+@Server.route("/") #Texto es igual al un archivo html, el cual no puede returnarse con solo abrirlo
+def Funcion_Init(): 
+    return Texto
+@Server.route("/InitRegistrarse")
+def Prim_Form():
+    return '''<form action = "/Registrarse" method = "post">
+    <label for = "nombre">Su nombre De usuario será</label>
+    <input type = "text" name="nombre" required><br>
+    <label for = "Cant_Archs">Cuantos archivos busca tener?</label>
+    <input type = "number" name="Cant_Archs"  required><br
+    ><button type = "sumbit">Enviar</button>
+    </form> '''
 
-@Server.route('/', methods=['POST'])
-def Cons_Nombre():
-    NombreArch = request.form["Archi"]  #El formulario debe llamarse como dice en los corchetes
-    NombreArch.lower()
-    try:
-      return redirect(url_for(NombreArch))    #Debe ir el nombre de una funcion entre los parentesis, la funcion no debe traer sus parentesis
-    except:
-       return "El archivo al que quieres dirigirte no existe"
-@Server.route("/rock")
-def rock():
-    return "<ul><li>Soda Estereo</li><li>Divididos</li><li>ACDC</li></ul>"
+@Server.route("/Registrarse", methods=["POST"]) #Al 
+def New_User():
+    global Nombre
+    global Cant_Archs
+    Nombre = request.form["nombre"]
+    Cant_Archs = request.form["Cant_Archs"]
+    
+    Nombre,Cant_Archs = Nombre.lower(), int(Cant_Archs)
+    Kamtda = ""
 
-@Server.route("/cumbia")
-def cumbia():
-    return "<ul><li>Pedro Capo</li><li>Juan Luis Guerra</li><li>Camilo</li></ul>"
+    for i in range(Cant_Archs):
+        Kamtda +=  f'''<label for = "nombreArch{str(i)}">NombreDeArchivo</label><input type = "text" name="nombreArch{str(i)}" required><br><label for = "tipoArch{str(i)}">TipoDeArchivo</label>
+    <input type = "text" name="tipoArch{str(i)}"  required value = "."><br>'''
+    return   '''<form action = "/FinRegistro" method = "post">''' + Kamtda + '''<button type = "sumbit">Enviar</button>
+    </form> '''
 
-@Server.route("/osos")
-def osos():
-    return "<ul><li>Polar</li><li>Pardo</li><li>Panda</li></ul>"
 
-@Server.route("/perros")
-def perros():
-    return "<ul><li>Caniche</li><li>Pastor Suizo</li><li>Chihuahua</li></ul>"
+@Server.route("/FinRegistro", methods = ["POST"])
+def Formar_Archivos():
+    
+    file = open("Usuarios.json", "r")
+    JonhSON = json.load(file)
+    New_Num_User = JonhSON["Cant_Users"]
+    New_Num_User += 1
+    
+    ListaArchivos = []
+    for i in range(Cant_Archs):
+        NameArch = request.form["nombreArch" + str(i)]
+        Parko = request.form["tipoArch" + str(i)]
+        NameArch += Parko
+        ListaArchivos.append(NameArch)
+    Diccion_User = {"Nombre": Nombre, "Nivel": "visitante", "Archivos": ListaArchivos}
+    
+    Ulo = {"U" + str(New_Num_User): Diccion_User}
+    file.close()
 
-@Server.route("/tortas")
-def tortas():
-    return "<ul><li>Bodas</li><li>Madre Selva</li><li>Margarita</li></ul>"
+    
+    JonhSON["U" + str(New_Num_User)] = Diccion_User
+    with open("Usuarios.json", "w") as file:
+      json.dump(JonhSON, file, indent=4)
+    
+    return "<h1> Te has registrado correctamente</h1>" + Texto
+@Server.route("/IniciarSesion")
+def Initium():
+    return Textos
 
-if __name__ == "main":    #Para que se ejecute solo y nos de bien los errores
-   Server.run(debug=True)
+if __name__ == "__main__":    #Para que se ejecute al clickear el play y no al escribir en CMD
+   Server.run(debug=True)  #Debug es para que de bien los errores
+   print("Listo")
+
+#Cant_Archivos es un str()
